41cee78e3c9dc388c48c1fc71044e56499bcfc9b
konstantin <refundsix@mail.ru>
2025-01-29 03:08:45 +0700

    promocodes

---
+++ components/CarStand.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="relative size-[39vw] max-sm:size-60 ml-[10vw]">
+    <img
+      src="../assets/images/test/car-2.svg"
+      alt="Car"
+      class="absolute top-[10vw] -left-[7.5vw] w-[40vw] max-sm:w-[240px] max-sm:top-[18vw] max-sm:-left-14"
+    />
+    <p
+      class="absolute top-[7.5vw] -left-[6.4vw] text-black z-10 font-semibold w-[10vw] text-[0.8vw] max-sm:text-xs max-sm:w-24 max-sm:-left-12 max-sm:top-4"
+    >
+      TPU-001S Glossy Swan White (SS)
+    </p>
+    <div class="absolute top-[3vw] -left-[7vw] max-sm:-left-16 max-sm:top-0">
+      <img
+        src="../assets/images/test/metal.svg"
+        alt="color"
+        class="w-[10.5vw] max-sm:w-28"
+      />
+      <img
+        src="../assets/images/icons/lines.svg"
+        alt="lines"
+        class="ml-7 mt-2 h-[7vw] max-sm:h-12 max-sm:ml-2"
+      />
+    </div>
+    <div
+      class="absolute inset-0 bg-[url('/circle.svg')] bg-center bg-contain bg-no-repeat"
+    ></div>
+    <div
+      class="size-[5.5vw] rounded-full bg-white border-2 absolute pt-[1vw] max-sm:pt-2 right-[11vw] top-[2.7vw] max-sm:size-16 max-sm:right-16 max-sm:-top-2"
+    >
+      <img
+        src="../assets/images/test/car-3.svg"
+        alt="selected_car"
+        class="size-[5.2vw] max-sm:size-16"
+      />
+    </div>
+    <p
+      class="text-[1.2vw] absolute -right-[2.3vw] top-[3.5vw] max-sm:right-2 max-sm:text-[9px] max-sm:top-0"
+    >
+      TPU-014S <br />Beetroot <br class="sm:hidden" />
+      Purple (SS)
+    </p>
+
+    <div
+      class="size-[5.5vw] rounded-full bg-white border-2 border-secondary max-sm:pt-2 absolute pt-[0.8vw] right-[3vw] top-[17vw] max-sm:size-16 max-sm:right-2 max-sm:top-[85px]"
+    >
+      <img
+        src="../assets/images/test/car-2.svg"
+        alt="selected_car"
+        class="size-[5.2vw] max-sm:size-16"
+      />
+    </div>
+    <p
+      class="text-[1.2vw] absolute -right-[12vw] top-[18vw] max-sm:-right-[55px] max-sm:text-[9px] max-sm:top-[92px]"
+    >
+      TPU-001S <br />
+      Glossy Swan <br class="sm:hidden" />
+      White (SS)
+    </p>
+    <div
+      class="size-[5.5vw] max-sm:size-16 rounded-full bg-white border-2 absolute pt-[1vw] max-sm:pt-2 right-[11vw] bottom-[2.5vw] max-sm:right-16 max-sm:bottom-0"
+    >
+      <img
+        src="../assets/images/test/car-4.svg"
+        alt="selected_car"
+        class="size-[5.2vw] max-sm:size-16"
+      />
+    </div>
+    <p
+      class="text-[1.2vw] absolute right-[4vw] bottom-[3.4vw] max-sm:right-3.5 max-sm:text-[9px] max-sm:bottom-4"
+    >
+      PU-015S <br />
+      Lake Blue
+    </p>
+  </div>
+</template>
---
+++ components/ChoiseColor.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col justify-end gap-[1.2vw] max-sm:gap-4">
+    <h1
+      class="text-[2.75vw] leading-[3.2vw] font-bold uppercase max-sm:text-2xl"
+    >
+      Марка Авто Пример
+    </h1>
+
+    <div class="w-full h-px bg-white"></div>
+
+    <h2 class="text-[2vw] max-sm:text-xl">Подобрать цвет</h2>
+
+    <select
+      id="filter"
+      class="px-4 py-5 max-sm:px-3 max-sm:py-3 w-full rounded bg-black text-white border text-lg border-white"
+    >
+      <option value="1" selected>Эксклюзивные</option>
+      <option value="2">Матовые</option>
+      <option value="3">Глянцевые</option>
+    </select>
+
+    <div class="grid grid-cols-5 w-full gap-4 max-sm:w-10/12">
+      <div class="rounded-full bg-red-500 size-[2.7vw] max-sm:size-10"></div>
+      <div class="rounded-full bg-orange-500 size-[2.7vw] max-sm:size-10"></div>
+      <div class="rounded-full bg-yellow-500 size-[2.7vw] max-sm:size-10"></div>
+      <div class="rounded-full bg-green-500 size-[2.7vw] max-sm:size-10"></div>
+      <div class="rounded-full bg-blue-500 size-[2.7vw] max-sm:size-10"></div>
+      <div class="rounded-full bg-purple-500 size-[2.7vw] max-sm:size-10"></div>
+      <div class="rounded-full bg-purple-900 size-[2.7vw] max-sm:size-10"></div>
+    </div>
+
+    <Button
+      class="font-bold text-white !text-[1.3vw] w-full h-[4.7vw] max-sm:h-[55px] max-sm:!text-base"
+    >
+      Выбрать
+    </Button>
+  </div>
+</template>
