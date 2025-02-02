649bf7b0ae925f233fae465aa2f2c2a334879577
konstantin <refundsix@mail.ru>
2025-02-02 20:19:17 +0700

    first prod version

--- server/api/colors/index.get.ts
+++ server/api/colors/index.get.ts
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 import { resolve } from "path";
 
 export default defineEventHandler(() => {
-  const path = resolve("./public/data/colors.json");
+  const path = resolve("./server/data/colors.json");
   const data = readFileSync(path, "utf-8");
   return JSON.parse(data);
 });
