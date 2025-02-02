c26b7258aafcc716c450ef49a39cf26cfd710888
konstantin <refundsix@mail.ru>
2025-02-02 20:56:52 +0700

    uiv1

--- server/api/colors/index.get.ts
+++ server/api/colors/index.get.ts
@@ -1,5 +1,8 @@
-import colorsData from "../../data/colors.json" assert { type: "json" };
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
 export default defineEventHandler(() => {
-  return colorsData;
+  const path = resolve("./server/data/colors.json");
+  const data = readFileSync(path, "utf-8");
+  return JSON.parse(data);
 });
